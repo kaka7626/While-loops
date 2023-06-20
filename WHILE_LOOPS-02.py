470333b66c18e0e460e6d9247414695fc1100ac0
kaka7626 <hoang7626@gmail.com>
2023-06-20 17:08:56 +0700

    ví dụ về break

---
+++ WHILE_LOOPS-02.py
@@ -0,0 +1,8 @@
+print('Nhập tên của bạn: ')
+name = input()
+while name == '':
+    print('Nhập tên của bạn pls!')
+    name = input()
+    if name == 'Long':
+        break
+print('Cảm ơn fren ^^')
