3cdd0c3538f92e9fa45e38321aefe07dc768e40c
kaka7626 <hoang7626@gmail.com>
2023-06-20 17:09:06 +0700

    ví dụ về continue

---
+++ WHILE_LOOPS-03.py
@@ -0,0 +1,7 @@
+number = 0
+while number < 30:
+    number = number + 1
+    if number == 24:
+        print('Đây là số tuổi hiện tại của Long: ' + str(number))
+        continue
+    print('Đây là số: ' + str(number))
